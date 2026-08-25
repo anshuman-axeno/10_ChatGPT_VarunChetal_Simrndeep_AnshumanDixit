85629326b61da669d972ad5fae00b2e3916a6b57
Anshuman <anshuman.dixit@axeno.co>
2026-08-25 12:32:12 +0530

    Like & Dislike working

--- scripts/chat.js
+++ scripts/chat.js
@@ -26,18 +26,32 @@ function loadConversation(id) {
       actions.className = "message-actions";
       actions.innerHTML = `
         <button class="action-btn copy-btn" ><img src="../assets/images/clipboard.svg" class="icon icon-grey"></button>
-        <button class="action-btn" id="like-btn"><img src="assets/images/thumb-up.svg" class="icon icon-grey"></button>
-        <button class="action-btn" id="dislike-btn"><img src="assets/images/thumb-down.svg" class="icon icon-grey"></button>
-        <button class="action-btn" id="regen-btn"><img src="assets/images/refresh.svg" class="icon icon-grey"></button>
-        <button class="action-btn" id="more-btn"><img src="assets/images/dots.svg" class="icon icon-grey"></button>
+        <button class="action-btn like-btn"><img src="../assets/images/thumb-up.svg" class="icon icon-grey"></button>
+        <button class="action-btn dislike-btn"><img src="../assets/images/thumb-down.svg" class="icon icon-grey"></button>
+        <button class="action-btn regen-btn"><img src="../assets/images/refresh.svg" class="icon icon-grey"></button>
+        <button class="action-btn more-btn" ><img src="../assets/images/dots.svg" class="icon icon-grey"></button>
       `;
       chatMessages.appendChild(actions);
+
+      const dislikeBtn=actions.querySelector('.dislike-btn');
 
       const copyBtn = actions.querySelector(".copy-btn");
       copyBtn.addEventListener("click", () => {
         navigator.clipboard.writeText(bubble.textContent);
-        alert('Copied to Clipboard');
+        //alert('Copied to Clipboard');
+        copyBtn.classList.add("copied");
+        setTimeout(() => copyBtn.classList.remove("copied"), 1000);
   });
+      const likeBtn=actions.querySelector('.like-btn');
+      likeBtn.addEventListener('click', ()=>{
+        //alert('Message Liked. You will get similar replies');
+        likeBtn.classList.toggle("liked"); 
+        dislikeBtn.classList.remove("disliked");
+      });
+      dislikeBtn.addEventListener('click', ()=>{
+        dislikeBtn.classList.toggle("disliked"); 
+        likeBtn.classList.remove("liked");
+      });
     }
   });
 }
